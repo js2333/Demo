95ffee3db99b13da40abcb0bed11eda5d72e55ec
fengh <fenghuo0203@outlook.com>
2019-05-20 14:21:51 +0800

    lc

---
+++ src/main/java/com/demo.java
@@ -0,0 +1,11 @@
+package main.java.com;
+
+/**
+ * @author 李畅
+ * @date 2019/5/20
+ */
+public class demo {
+    public static void main(String[] args) {
+        System.out.println("江帅是阴阳师");
+    }
+}
